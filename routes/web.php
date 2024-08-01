cd6d0c64de81df0ecb9365665974243c7d796569
Wilfried Ouattara <ouattaradattejeanwilfried@gmail.com>
2024-08-01 14:04:31 +0000

    Create user authantification and register and login views

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/register', [UserController::class, 'showRegistrationForm']);
+Route::post('/register', [UserController::class, 'register'])->name('register');
+
+Route::get('/login', [UserController::class, 'showLoginForm']);
+Route::post('/login', [UserController::class, 'login'])->name('login');
 
 Route::get('/', function () {
     return view('welcome');
